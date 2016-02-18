49cf08cd81b3e022aa767731e9523cfed56fd8c1
Andriy Buday <A.BUDAY@iaea.org>
2016-02-18 09:08:55 +0100

    Added MVC 6 and some basic form with validation

---
+++ TheWorld/src/TheWorld/Services/IMailService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public interface IMailService
+    {
+        bool SendMail(string to, string from, string subject, string body);
+    }
+
+    public class DebugMailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string body)
+        {
+            Debug.WriteLine($"Sending mail: To: {to}, Subject {subject}");
+            return true;
+        }
+    }
+}

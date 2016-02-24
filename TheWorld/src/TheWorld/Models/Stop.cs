350aeafed89728032d23935f732cfb17f86cc709
Andriy Buday <A.BUDAY@iaea.org>
2016-02-24 15:23:19 +0100

    Added DB migrations

---
+++ TheWorld/src/TheWorld/Models/Stop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Longitude { get; set; }
+        public long Latitude { get; set; }
+        public DateTime Arrival { get; set; }
+        public int Order { get; set; }
+    }
+}
